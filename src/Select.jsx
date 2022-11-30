527f3d6f5cd926c8ff91dc320e92f6daecae5c95
Manik Hosen <rahathossenmanik@gmail.com>
2022-11-30 20:46:11 +0600

    Select Tag Version

--- src/Select.jsx
+++ src/Select.jsx
@@ -1,9 +1,17 @@
+import { castArray } from 'lodash-es';
 import React from 'react';
 const Select = () => {
-	return React.createElement('input', {
-		type: 'text',
-		placeholder: 'Please Select',
-	});
+	return React.createElement(
+		'select',
+		{
+			type: 'text',
+			name: 'cars',
+			placeholder: 'Please Select',
+		},
+		[1, 2, 3].map((item) => {
+			React.createElement('option', { value: item }, item);
+		})
+	);
 };
 
 export default Select;
