c5bbac9b9fcb176754d52177e12a448372eced7b
Manik Hosen <rahathossenmanik@gmail.com>
2022-11-30 21:43:47 +0600

    Simple Dropdown

--- src/Select.jsx
+++ src/Select.jsx
@@ -1,16 +1,56 @@
-import { castArray } from 'lodash-es';
 import React from 'react';
 const Select = () => {
 	return React.createElement(
-		'select',
+		'div',
 		{
+			className: 'react-jsx-select-wrapper',
+			style: { position: 'relative' },
+		},
+		React.createElement('input', {
 			type: 'text',
 			name: 'cars',
 			placeholder: 'Please Select',
-		},
-		[1, 2, 3].map((item) => {
-			React.createElement('option', { value: item }, item);
-		})
+			className: 'form-control react-jsx-select-input',
+		}),
+		React.createElement(
+			'div',
+			{
+				className: 'react-jsx-select-dropdown',
+				style: {
+					backgroundColor: '#fff',
+					position: 'absolute',
+					left: 0,
+					right: 0,
+					zIndex: 1000,
+				},
+			},
+			React.createElement(
+				'ul',
+				{
+					className: 'react-jsx-select-list',
+					style: {
+						border: '1px solid #ccc',
+						paddingLeft: 0,
+						listStyle: 'none',
+					},
+				},
+				[<p>1</p>, <p>2</p>, <p>3</p>, <p>4</p>, <p>5</p>].map((item, i) =>
+					React.createElement(
+						'li',
+						{
+							key: i,
+							className: 'react-jsx-select-list-item',
+							style: {
+								paddingLeft: '10px',
+								paddingRight: '10px',
+								listStyle: 'none',
+							},
+						},
+						item
+					)
+				)
+			)
+		)
 	);
 };
 
